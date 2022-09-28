ab580d72931c82710b1f0333aabaa4c4e56fed6d
nwebpro <nwebpro.work@gmail.com>
2022-09-29 02:17:46 +0600

    add to break time button after click button active

--- src/Components/Profile/Break.js
+++ src/Components/Profile/Break.js
@@ -5,11 +5,11 @@ const Break = ({addBreakTime}) => {
         <section className='mb-10'>
             <h3 className='my-5 font-medium text-xl text-black'>Add A Break</h3>
             <div className='flex justify-between bg-slate-200 p-5 rounded-xl'>
-                <button onClick={() => addBreakTime(10)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white">10m</button>
-                <button onClick={() => addBreakTime(20)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white">20m</button>
-                <button onClick={() => addBreakTime(30)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white">30m</button>
-                <button onClick={() => addBreakTime(40)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white">40m</button>
-                <button onClick={() => addBreakTime(50)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white">50m</button>
+                <button onClick={() => addBreakTime(10)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white active:bg-purple-500 focus:bg-purple-500 active:text-white focus:text-white">10m</button>
+                <button onClick={() => addBreakTime(20)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white active:bg-purple-500 focus:bg-purple-500 active:text-white focus:text-white">20m</button>
+                <button onClick={() => addBreakTime(30)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white active:bg-purple-500 focus:bg-purple-500 active:text-white focus:text-white">30m</button>
+                <button onClick={() => addBreakTime(40)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white active:bg-purple-500 focus:bg-purple-500 active:text-white focus:text-white">40m</button>
+                <button onClick={() => addBreakTime(50)} className="w-12 h-12 rounded-full mr-2 text-xs font-bold bg-white active:bg-purple-500 focus:bg-purple-500 active:text-white focus:text-white">50m</button>
             </div>
         </section>
     );
